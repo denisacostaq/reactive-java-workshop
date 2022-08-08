3fd73bb6c3e60c5239c60281f56fe7521bdbbfe2
Alvaro Denis <denisacostaq@gmail.com>
2022-08-08 22:35:55 +0300

    Solves Exercise3

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise3 {
 
@@ -8,13 +10,15 @@ public class Exercise3 {
 
         // Use ReactiveSources.intNumbersFlux()
 
-        // Get all numbers in the ReactiveSources.intNumbersFlux stream
-        // into a List and print the list and its size
-        // TODO: Write code here
+        System.out.println("Get all numbers in the ReactiveSources.intNumbersFlux stream " +
+                "into a List and print the list and its size");
+        List<Integer> l = ReactiveSources.intNumbersFlux().toStream().collect(Collectors.toList());
+        System.out.println(l);
+        System.out.println(l.size());
 
 
-        System.out.println("Press a key to end");
-        System.in.read();
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
 
 }
