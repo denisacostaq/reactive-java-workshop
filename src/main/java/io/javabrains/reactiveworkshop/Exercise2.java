3844e6065fb3306039a4f836e55f5e9098a6d29b
Alvaro Denis <denisacostaq@gmail.com>
2022-08-08 22:19:32 +0300

    Solves Exercise2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -8,11 +8,11 @@ public class Exercise2 {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
-        // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
 
-        // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        System.out.println("Print all users in the ReactiveSources.userFlux stream");
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
